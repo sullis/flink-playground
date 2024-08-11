130eee373b41fc91fa58c65e29f7280c1947a8ac
sullis <github@seansullivan.com>
2024-08-11 11:43:39 -0700

    use Flink Mini Cluster

---
+++ src/test/java/io/github/sullis/flink/playground/FlinkMiniClusterTest.java
@@ -0,0 +1,26 @@
+package io.github.sullis.flink.playground;
+
+import java.util.stream.Stream;
+import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+import org.apache.flink.streaming.util.TestStreamEnvironment;
+import org.apache.flink.test.junit5.MiniClusterExtension;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.testcontainers.containers.Network;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+
+@Testcontainers
+@ExtendWith(MiniClusterExtension.class)
+public class FlinkMiniClusterTest {
+
+  @Test
+  public void testHappyPath() {
+    StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
+    assertThat(env).isInstanceOf(TestStreamEnvironment.class);
+  }
+}
