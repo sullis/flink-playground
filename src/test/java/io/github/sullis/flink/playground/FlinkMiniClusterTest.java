8f802701e1e7324736083dc93f2bcec3b77cd6c5
sullis <github@seansullivan.com>
2024-08-23 09:22:07 -0700

    cleanup imports

--- src/test/java/io/github/sullis/flink/playground/FlinkMiniClusterTest.java
+++ src/test/java/io/github/sullis/flink/playground/FlinkMiniClusterTest.java
@@ -1,14 +1,10 @@
 package io.github.sullis.flink.playground;
 
-import java.util.stream.Stream;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
 import org.apache.flink.streaming.util.TestStreamEnvironment;
 import org.apache.flink.test.junit5.MiniClusterExtension;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.junit.jupiter.api.extension.ExtendWith;
-import org.testcontainers.containers.Network;
-import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
 
 import static org.assertj.core.api.Assertions.assertThat;
